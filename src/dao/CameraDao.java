cebc84f99d2c7700d9e169ed3820e90f62a6c5e2
muerschi <ninamuerschberger@web.de>
2017-05-16 15:41:12 +0200

    Hinzufügen der getCamerasOfUser() und deleteCamerasOfUsers() zum bearbeiten der Kameras eines bestimmten Nutzers

--- src/dao/CameraDao.java
+++ src/dao/CameraDao.java
@@ -3,10 +3,13 @@ package dao;
 import java.util.List;
 
 import model.Camera;
+import model.User;
 
 public interface CameraDao {
 	public void save(Camera camera);
 	public void delete(Long id);
 	public Camera get(Long id);
 	public List<Camera> list();
+	public List<Camera> getCamerasOfUser(User user);
+	public void saveCamerasForUser(User user, List<Camera> cameras);
 }
